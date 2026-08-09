c958404fa162c0b10432718c688b8d68b55ffa2f
elibuilds <elik.amuzu@gmail.com>
2026-08-09 12:19:14 +0000

    cartpole and blackjack env

---
+++ cartpole.py
@@ -0,0 +1,23 @@
+#Understanding the basic usage of gym in rl
+import gymnasium as gym 
+
+env = gym.make('CartPole-v1', render_mode = "human")
+
+observation, info = env.reset()
+
+print(f"Starting observation: {observation}")
+
+episode_over = False
+total_reward = 0
+
+while not episode_over:
+    action = env.action_space.sample()
+
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    total_reward += reward
+    episode_over = terminated or truncated
+
+print(f"Episode finished! Total reward: {total_reward}")
+env.close()
+
